58d084234c241858c0714f4165e74d8090c6e0f8
obithree <projectofsavepiece0706@gmail.com>
2020-08-08 12:29:32 +0900

    fix(entity):種族値classの骨組み作成

--- app/pokemon_stats_judge/entity/pokemon.py
+++ app/pokemon_stats_judge/entity/pokemon.py
@@ -1,5 +1,16 @@
 import dataclasses
 
+
+@dataclasses.dataclass(frozen=True)
+class PokemonBaseStats:
+    pokemon_name: str
+    pokemon_base_hp: int
+    pokemon_base_atk: int
+    pokemon_base_def: int
+    pokemon_base_spcl_atk: int
+    pokemon_base_spcl_def: int
+    pokemon_base_speed: int
+
 @dataclasses.dataclass(frozen=True)
 class Pokemon:
     pokemon_name: str
@@ -11,3 +22,4 @@ class Pokemon:
     pokemon_stat_spcl_atk: int
     pokemon_stat_spcl_def: int
     pokemon_stat_speed: int
+    pokemon_base_stats: PokemonBaseStats
--- app/pokemon_stats_judge/test/entity/test_pokemon.py
+++ app/pokemon_stats_judge/test/entity/test_pokemon.py
@@ -1,6 +1,6 @@
 import unittest
 from pokemon_stats_judge.entity.pokemon import Pokemon
-
+from pokemon_stats_judge.entity.pokemon import PokemonBaseStats
 
 class TestPokemon(unittest.TestCase):
     """test class of pokemon entity
@@ -20,7 +20,23 @@ class TestPokemon(unittest.TestCase):
         pokemon_stat_spcl_def = 138
         pokemon_stat_speed = 113
         # Todo:ポケモンの種族値取得も含めてテスト
-        # pokemon_base_stats = {}
+        pokemon_base_hp = 62
+        pokemon_base_atk = 73
+        pokemon_base_def = 55
+        pokemon_base_spcl_atk = 56
+        pokemon_base_spcl_def = 52
+        pokemon_base_speed = 84
+        pokemon_base_stats = PokemonBaseStats(
+            pokemon_name,
+            pokemon_base_hp,
+            pokemon_base_atk,
+            pokemon_base_def,
+            pokemon_base_spcl_atk,
+            pokemon_base_spcl_def,
+            pokemon_base_speed
+        )
+
+
         test_pokemon = Pokemon(
                                 pokemon_name,
                                 pokemon_stat_level,
@@ -30,6 +46,8 @@ class TestPokemon(unittest.TestCase):
                                 pokemon_stat_def,
                                 pokemon_stat_spcl_atk,
                                 pokemon_stat_spcl_def,
-                                pokemon_stat_speed
+                                pokemon_stat_speed,
+                                pokemon_base_stats
         ) 
         assert test_pokemon.pokemon_name is pokemon_name
+        assert test_pokemon.pokemon_base_stats.pokemon_base_hp is pokemon_base_hp
