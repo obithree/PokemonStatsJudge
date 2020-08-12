78436611bf13943e4683145226046359ccb24bc5
obithree <projectofsavepiece0706@gmail.com>
2020-08-12 14:29:00 +0900

    fix(adds):努力値のテストコードlint対応

--- app/pokemon_stats_judge/test/entity/test_pokemon_evs.py
+++ app/pokemon_stats_judge/test/entity/test_pokemon_evs.py
@@ -1,16 +1,17 @@
-import unittest
+"""Test EffortValues"""
 import pytest
-from pokemon_stats_judge.entity.pokemon import Pokemon
-from pokemon_stats_judge.entity.pokemon import PokemonBaseStats
 from pokemon_stats_judge.entity.pokemon import PokemonEffortValues
-from pokemon_stats_judge.entity.pokemon import PokemonIndividualValues
 from pokemon_stats_judge.entity.Exception.pokemon_exception import InvalidArgumentTypeException
 from pokemon_stats_judge.entity.Exception.pokemon_exception import InvalidEffortValueException
 from pokemon_stats_judge.entity.Exception.pokemon_exception import InvalidEffortValuesException
 
 
-class TestPokemonEffortValues(object):
+class TestPokemonEffortValues:
+    """努力値のテスト用クラス
+    """
     def test_create_evs(self, test_evs):
+        """正しい値を入れて作成できるか確認する。
+        """
         evs = PokemonEffortValues(
             hp=test_evs['hp'],
             phys_atk=test_evs['phys_atk'],
@@ -19,9 +20,11 @@ class TestPokemonEffortValues(object):
             spcl_def=test_evs['spcl_def'],
             speed=test_evs['speed']
         )
-        assert evs.hp is 252
+        assert evs.hp == 252
 
     def test_get_dict(self, test_evs):
+        """get_dictメソッドでdict型を返すかテストする。
+        """
         evs = PokemonEffortValues(
             hp=test_evs['hp'],
             phys_atk=test_evs['phys_atk'],
@@ -33,7 +36,9 @@ class TestPokemonEffortValues(object):
         evs_dict = evs.get_dict()
         assert isinstance(evs_dict, dict)
 
-    def test_is_valid(self, test_evs):
+    def test_is_valid(self):
+        """素早さに入るはずのないlist型を渡して"InvalidArgumentTypeException"が出るか確認する。
+        """
         with pytest.raises(InvalidArgumentTypeException):
             PokemonEffortValues(
                 hp=252,
@@ -44,17 +49,21 @@ class TestPokemonEffortValues(object):
                 speed=[252]
             )
 
-    def test_schema_evs_value(self, test_evs):
+    def test_schema_evs_value(self):
+        """努力値の最大値255を超えた値を入れるとエラーが出るか確認する。
+        """
         with pytest.raises(InvalidEffortValueException):
             PokemonEffortValues(
                 hp=300,
-                phys_atk=test_evs['phys_atk'],
-                phys_def=test_evs['phys_def'],
-                spcl_atk=test_evs['spcl_atk'],
-                spcl_def=test_evs['spcl_def'],
-                speed=test_evs['speed']
+                phys_atk=0,
+                phys_def=0,
+                spcl_atk=0,
+                spcl_def=0,
+                speed=0
             )
-    def test_schema_evs_values(self, test_evs):
+    def test_schema_evs_values(self):
+        """努力値の合計が510を超えていないか確認する。
+        """
         with pytest.raises(InvalidEffortValuesException):
             PokemonEffortValues(
                 hp=252,
