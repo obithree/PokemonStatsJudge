8fbeef40e5c76b7e7cf2bf5caba05ba91b366387
obithree <projectofsavepiece0706@gmail.com>
2020-08-12 14:34:22 +0900

    fix(lint)個体値のテストコードlint対応2

--- app/pokemon_stats_judge/test/entity/test_pokemon_ivs.py
+++ app/pokemon_stats_judge/test/entity/test_pokemon_ivs.py
@@ -1,7 +1,5 @@
 """Test IndividualValues"""
 import pytest
-from pokemon_stats_judge.entity.pokemon import Pokemon
-from pokemon_stats_judge.entity.pokemon import PokemonBaseStats
 from pokemon_stats_judge.entity.pokemon import PokemonIndividualValues
 from pokemon_stats_judge.entity.Exception.pokemon_exception import InvalidArgumentTypeException
 from pokemon_stats_judge.entity.Exception.pokemon_exception import InvalidIndividualValueException
@@ -21,7 +19,7 @@ class TestPokemonIndividualValues:
             spcl_def=test_ivs['spcl_def'],
             speed=test_ivs['speed']
         )
-        assert ivs.hp[0] is 23
+        assert ivs.hp[0] == 23
 
     def test_get_dict(self, test_ivs):
         """get_dictメソッドでdict型を返すかテストする。
@@ -37,28 +35,28 @@ class TestPokemonIndividualValues:
         ivs_dict = ivs.get_dict()
         assert isinstance(ivs_dict, dict)
 
-    def test_is_valid(self, test_ivs):
+    def test_is_valid(self):
         """入るはずのないlist型を渡して"InvalidArgumentTypeException"が出るか確認する。
         """
         with pytest.raises(InvalidArgumentTypeException):
             PokemonIndividualValues(
-                hp=[7,8],
-                phys_atk=[7,8],
-                phys_def=[7,8],
-                spcl_atk=[7,8],
-                spcl_def=[7,8],
-                speed=39
+                hp=[7, 8],
+                phys_atk=[7, 8],
+                phys_def=[7, 8],
+                spcl_atk=[7, 8],
+                spcl_def=[7, 8],
+                speed=31
             )
 
-    def test_schema_ivs_value(self, test_ivs):
+    def test_schema_ivs_value(self):
         """個体値の最大の31を超えた数字を入れた場合、"InvalidIndividualValueException"が出るか確認する。
         """
         with pytest.raises(InvalidIndividualValueException):
             PokemonIndividualValues(
-                hp=[7,8],
-                phys_atk=[7,8],
-                phys_def=[7,8],
-                spcl_atk=[7,8],
-                spcl_def=[7,8],
+                hp=[7, 8],
+                phys_atk=[7, 8],
+                phys_def=[7, 8],
+                spcl_atk=[7, 8],
+                spcl_def=[7, 8],
                 speed=[39]
             )
