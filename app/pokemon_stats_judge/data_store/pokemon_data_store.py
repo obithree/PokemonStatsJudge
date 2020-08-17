069f7ad6815058198956bf3e433e3e82737c917b
obithree <projectofsavepiece0706@gmail.com>
2020-08-17 20:55:10 +0900

    fix(datastore):try_exceptでErrorハンドリング追加(骨組み)

--- app/pokemon_stats_judge/data_store/pokemon_data_store.py
+++ app/pokemon_stats_judge/data_store/pokemon_data_store.py
@@ -20,7 +20,23 @@ class PokemonDataStore(PokemonRepository):
         """
         pokemon_name = pokemon.pokemon_name
         base_stats_json_path = f'{os.path.dirname(__file__)}/pokemon_base_stats.json'
-        with open(base_stats_json_path, encoding="utf-8") as base_stats_json:
-            base_stats_dict = json.load(base_stats_json)
-            pokemon_base_stats = PokemonBaseStats(**base_stats_dict[pokemon_name]['base_stats']['normal_form'])
-            return pokemon_base_stats
+        try:
+            with open(base_stats_json_path, encoding="utf-8") as base_stats_json:
+                base_stats_dict = json.load(base_stats_json)
+                pokemon_base_stats = PokemonBaseStats(**base_stats_dict[pokemon_name]['base_stats']['normal_form'])
+        except FileNotFoundError as error_m:
+            print(error_m)
+            return
+        except json.decoder.JSONDecodeError as error_m:
+            print(error_m)
+            return
+        except UnicodeDecodeError as error_m:
+            print(error_m)
+            return
+        except UnicodeDecodeError as error_m:
+            print(error_m)
+            return
+        except KeyError as error_m:
+            print(f'{error_m} is not exist in Pokemon List')
+            return
+        return pokemon_base_stats
