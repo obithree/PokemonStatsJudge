4c591f6d517f0aead0debae4a977a4d180451194
obithree <projectofsavepiece0706@gmail.com>
2020-08-21 22:15:14 +0900

    fix(use_case):use_caseのエラーハンドル追加

--- app/pokemon_stats_judge/test/use_case/test_pokemon_use_case.py
+++ app/pokemon_stats_judge/test/use_case/test_pokemon_use_case.py
@@ -20,3 +20,18 @@ class TestPokemonUseCase:
         judged_pokemon = pokemon_use_case.pokemon_status_judge(request_object=test_pokemon_stats)
         assert judged_pokemon['pokemon_evs']['hp'] == 220
         assert judged_pokemon['pokemon_ivs']['hp'] == [31]
+
+    def test_failed_judge(self, test_pokemon_stats, test_base_stats):
+        """誤ったポケモンの実値を渡す。。
+        response_objectに失敗したメッセージが入っているか確認する。
+        """
+        mock_pokemon_data_store = mock.Mock()
+        mock_pokemon_repository = mock_pokemon_data_store(PokemonRepository)
+        failed_test_pokemon_stats = test_pokemon_stats
+        failed_test_pokemon_stats['pokemon_name'] =  'けつばん'
+        failed_test_pokemon_stats['pokemon_stat_hp'] = 900
+        mock_pokemon_repository.get_base_stats.return_value = PokemonBaseStats(**test_base_stats)
+        pokemon_use_case = PokemonUseCase(mock_pokemon_repository)
+        judged_pokemon = pokemon_use_case.pokemon_status_judge(request_object=failed_test_pokemon_stats)
+        print(judged_pokemon['message'])
+        assert judged_pokemon['message'].args == ('hp', 2540)
--- app/pokemon_stats_judge/use_case/pokemon_use_case.py
+++ app/pokemon_stats_judge/use_case/pokemon_use_case.py
@@ -2,6 +2,11 @@
 # Todo: import方法の統一(全体を一つのModuleとして取れるようにする)
 from pokemon_stats_judge.entity.pokemon import Pokemon
 from pokemon_stats_judge.repository.pokemon_repository import PokemonRepository
+from pokemon_stats_judge.error import PokemonError
+from pokemon_stats_judge.entity.error import InvalidArgumentTypeError
+from pokemon_stats_judge.entity.error import InvalidEffortValueError
+from pokemon_stats_judge.entity.error import InvalidEffortValuesError
+from pokemon_stats_judge.entity.error import InvalidIndividualValueError
 
 
 class PokemonUseCase:
@@ -15,8 +20,16 @@ class PokemonUseCase:
     def pokemon_status_judge(self, request_object: dict) -> dict:
         """ポケモンの個体値と努力値を推測し、dictで返す。
         """
-        pokemon = Pokemon(**request_object)
-        pokemon_base_stats = self.pokemon_repository.get_base_stats(pokemon)
-        pokemon_got_base_stats = pokemon.get_pokemon_by_replaced_base_stats(pokemon_base_stats)
-        judged_pokemon = pokemon_got_base_stats.get_pokemon_by_estimated_ivs_and_evs()
+        try:
+            pokemon = Pokemon(**request_object)
+            pokemon_base_stats = self.pokemon_repository.get_base_stats(pokemon)
+            pokemon_got_base_stats = pokemon.get_pokemon_by_replaced_base_stats(pokemon_base_stats)
+        except (InvalidArgumentTypeError, PokemonError) as error_message:
+            response_object = {"message": error_message}
+            return response_object
+        try:
+            judged_pokemon = pokemon_got_base_stats.get_pokemon_by_estimated_ivs_and_evs()
+        except (InvalidEffortValuesError, InvalidEffortValueError, InvalidIndividualValueError) as error_message:
+            response_object = {"message": error_message}
+            return response_object
         return judged_pokemon.get_dict()
